680f1668a552834bae0a0d56b64084a73c0f6ea3
John Codeos <johncodeos@gmail.com>
2021-10-21 13:18:06 +0300

    Google Sign-In SDK v6.0.0 support

--- GoogleSignInExample/AppDelegate.swift
+++ GoogleSignInExample/AppDelegate.swift
@@ -32,9 +32,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        return GIDSignIn.sharedInstance().handle(url)
+        return GIDSignIn.sharedInstance.handle(url)
     }
 
 
 }
-
--- GoogleSignInExample/LoginViewController.swift
+++ GoogleSignInExample/LoginViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 class LoginViewController: UIViewController {
     @IBOutlet var googleLoginBtn: UIButton!
     
-    var googleSignIn = GIDSignIn.sharedInstance()
+    var googleSignIn = GIDSignIn.sharedInstance
     var googleId = ""
     var googleIdToken = ""
     var googleFirstName = ""
@@ -25,10 +25,14 @@ class LoginViewController: UIViewController {
         self.googleLoginBtn.layer.cornerRadius = 10.0
         
         // Sign out (LogOut User from Google Account)
-        // self.googleSignIn?.signOut()
+        // self.googleSignIn.signOut()
         
-        // Disconnect (User calls this when they want to delete their account from your app)
-        // self.googleSignIn?.disconnect()
+        // Disconnect (User calls this when they want to delete their account from your app. Usually, this can be called when you have the option 'Delete my account' on the user's profile settings)
+//        self.googleSignIn.disconnect { error in
+//            if error == nil {
+        // Call your backend server to delete user's info after they requested to delete their account
+//            }
+//        }
     }
     
     @IBAction func googleLoginBtnAction(_ sender: UIButton) {
@@ -36,10 +40,40 @@ class LoginViewController: UIViewController {
     }
     
     func googleAuthLogin() {
-        self.googleSignIn?.presentingViewController = self
-        self.googleSignIn?.clientID = "YOUR_CLIENT_ID"
-        self.googleSignIn?.delegate = self
-        self.googleSignIn?.signIn()
+        let googleConfig = GIDConfiguration(clientID: "CLIENT_ID")
+        self.googleSignIn.signIn(with: googleConfig, presenting: self) { user, error in
+            if error == nil {
+                guard let user = user else {
+                    print("Uh oh. The user cancelled the Google login.")
+                    return
+                }
+                let userId = user.userID ?? ""
+                print("Google User ID: \(userId)")
+                self.googleId = userId
+                
+                let userIdToken = user.authentication.idToken ?? ""
+                print("Google ID Token: \(userIdToken)")
+                self.googleIdToken = userIdToken
+                
+                let userFirstName = user.profile?.givenName ?? ""
+                print("Google User First Name: \(userFirstName)")
+                self.googleFirstName = userFirstName
+                
+                let userLastName = user.profile?.familyName ?? ""
+                print("Google User Last Name: \(userLastName)")
+                self.googleLastName = userLastName
+                
+                let userEmail = user.profile?.email ?? ""
+                print("Google User Email: \(userEmail)")
+                self.googleEmail = userEmail
+                
+                let googleProfilePicURL = user.profile?.imageURL(withDimension: 150)?.absoluteString ?? ""
+                print("Google Profile Avatar URL: \(googleProfilePicURL)")
+                self.googleProfileURL = googleProfilePicURL
+                
+                self.performSegue(withIdentifier: "detailseg", sender: self)
+            }
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -55,40 +89,4 @@ class LoginViewController: UIViewController {
     }
 }
 
-extension LoginViewController: GIDSignInDelegate {
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        guard let user = user else {
-            print("Uh oh. The user cancelled the Google login.")
-            return
-        }
-        let userId = user.userID ?? ""
-        print("Google User ID: \(userId)")
-        self.googleId = userId
-        
-        let userIdToken = user.authentication.idToken ?? ""
-        print("Google ID Token: \(userIdToken)")
-        self.googleIdToken = userIdToken
-        
-        let userFirstName = user.profile.givenName ?? ""
-        print("Google User First Name: \(userFirstName)")
-        self.googleFirstName = userFirstName
-        
-        let userLastName = user.profile.familyName ?? ""
-        print("Google User Last Name: \(userLastName)")
-        self.googleLastName = userLastName
-        
-        let userEmail = user.profile.email ?? ""
-        print("Google User Email: \(userEmail)")
-        self.googleEmail = userEmail
-        
-        let googleProfilePicURL = user.profile.imageURL(withDimension: 150)?.absoluteString ?? ""
-        print("Google Profile Avatar URL: \(googleProfilePicURL)")
-        self.googleProfileURL = googleProfilePicURL
-        
-        self.performSegue(withIdentifier: "detailseg", sender: self)
-    }
-    
-    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
-        // Call your backend server to delete user's info after they requested to delete their account
-    }
-}
+
